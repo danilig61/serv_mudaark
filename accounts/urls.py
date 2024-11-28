12a5cb221ad528a24c1a6648fc91175c42687f6e
Даниил Игнатьев <ignatev_daniil61@mail.ru>
2024-11-28 14:57:33 +0300

    google fix urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, LoginAPIView, LogoutAPIView, RegisterAPIView, VerifyEmailAPIView, SetPasswordAPIView, \
-    MainAPIView, GoogleAuthAPIView, ResendVerificationCodeAPIView
+    MainAPIView, ResendVerificationCodeAPIView, GoogleLoginAPIView, GoogleCallbackAPIView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
