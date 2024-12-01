dbdbc09edf8c0e08c74b178421d83dc9448a7090
Даниил Игнатьев <ignatev_daniil61@mail.ru>
2024-12-01 20:44:45 +0300

    поправил редирект при гугл автор

--- mudaark/settings.py
+++ mudaark/settings.py
@@ -126,13 +126,17 @@ SOCIALACCOUNT_LOGIN_REDIRECT_URL = '/files/my_files/'
 
 
 SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_user',  # Ищет существующего пользователя
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',  # This ensures the UID is extracted.
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',  # Создает пользователя, если он не существует
+    'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['openid', 'email', 'profile']
 SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['id', 'email', 'name']
