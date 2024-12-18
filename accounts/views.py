440bae77b786fc68691fd67f4a4ba986e1352a70
Даниил Игнатьев <ignatev_daniil61@mail.ru>
2024-12-18 14:27:29 +0300

    coviit

--- accounts/views.py
+++ accounts/views.py
@@ -371,12 +371,11 @@ class YandexLoginAPIView(APIView):
             logger.info("Starting YandexLoginAPIView post method")
 
             access_token = request.data.get("access_token")
-            state = request.data.get("state")
-            if not access_token or not state:
-                logger.error("Access token and state are required")
+            if not access_token:
+                logger.error("Access token are required")
                 return Response({
                     'status_code': status.HTTP_400_BAD_REQUEST,
-                    'error': 'Access token and state are required',
+                    'error': 'Access token are required',
                 }, status=status.HTTP_400_BAD_REQUEST)
 
             logger.info("Access token received, verifying with Yandex")
